e5d4b10d57a8c21702834618c850a6986ebc7adb
d4nQw3rty <danlinmoy@gmail.com>
2022-12-05 15:56:32 -0400

    CREATE ANIMALS TABLE

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals (
-    name varchar(100)
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT NOT NULL CHECK (escape_attempts >= 0),
+    neutered BOOLEAN NOT NULL,
+    weight_kg FLOAT(2) NOT NULL CHECK (weight_kg >= 0.0), 
 );
