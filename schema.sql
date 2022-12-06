0d355ee473cf725695baa201eb90f9210492cb85
d4nQw3rty <danlinmoy@gmail.com>
2022-12-06 15:22:23 -0400

    Add species to animals table

--- schema.sql
+++ schema.sql
@@ -4,8 +4,9 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL CHECK (escape_attempts >= 0),
+    escape_attempts INT NOT NULL CHECK,
     neutered BOOLEAN NOT NULL,
-    weight_kg FLOAT(2) NOT NULL CHECK (weight_kg >= 0),
+    weight_kg FLOAT(2) NOT NULL CHECK,
+    species VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 );
