c6712b79c7f5a989ca531620b5935958f43d4ad0
d4nQw3rty <danlinmoy@gmail.com>
2022-12-07 08:10:16 -0400

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,9 +4,10 @@ CREATE TABLE animals (
     id INT GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(255) NOT NULL,
     date_of_birth DATE NOT NULL,
-    escape_attempts INT NOT NULL CHECK,
+    escape_attempts INT NOT NULL,
     neutered BOOLEAN NOT NULL,
-    weight_kg FLOAT(2) NOT NULL CHECK,
-    species VARCHAR(255) NOT NULL,
+    weight_kg FLOAT(2) NOT NULL,   
     PRIMARY KEY (id)
 );
+
+ALTER TABLE animals ADD species VARCHAR(255);
